8b15b6630a60ef1b0e97c618db52aabf64f07454
Jaqueline Mera <jaqueline.geo.mera@gmail.com>
2024-09-06 17:16:27 -0600

    sql

---
+++ SQL/top_10_retrasos_aeropuertos.sql
@@ -0,0 +1,18 @@
+--- Aeropuertos ---
+----Top 10 aeropuertos con más retrasos y su promedio de tiempo (en min)---
+SELECT
+  ORIGIN,
+  ORIGIN_CITY,
+  COUNT(*) AS total_retrasos,
+  ROUND(AVG(TOTAL_DELAY) ,2)AS promedio_retraso
+FROM
+  `proyecto4-vuelos.procesos_vuelos.flights_consolidado`
+WHERE
+  TOTAL_DELAY > 0  -- Filtramos solo los retrasos (TOTAL_DELAY > 0)
+GROUP BY
+  ORIGIN,
+  ORIGIN_CITY
+ORDER BY
+  total_retrasos DESC
+LIMIT
+  10;
